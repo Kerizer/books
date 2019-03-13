7981df69895b2339748fba1697a7b0c603a77593
Pavlo Pakholka <pavlo.pakholka@leongaming.com>
2019-03-13 14:20:27 +0200

    Books list basic template. Still in parents component

--- src/components/SingleBestSellersList/SingleBestSellersList.js
+++ src/components/SingleBestSellersList/SingleBestSellersList.js
@@ -7,8 +7,16 @@ class SingleBestSellersList extends React.Component {
 	}
 
 	render() {
+		let { listInfo } = this.props;
+		if (!listInfo) {
+			return <div>TODO: if isLoading false - handle somehow</div>;
+		}
 		return <div>
-			<h3>{this.props.listInfo && this.props.listInfo.display_name}</h3>
+			<h3>{listInfo.display_name}</h3>
+			{listInfo.books.map(book => <div key={book.primary_isbn13}>
+				<h4>{book.title}</h4>
+				<p>{book.description}</p>
+			</div>)}
 		</div>
 	}
 }
